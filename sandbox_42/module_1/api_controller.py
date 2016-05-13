784423b4cc80f75d5d79c6a05416588a4b5844a0
m-hintz-42 <michael.c.hintz@gmail.com>
2016-05-13 16:50:30 -0500

    Playing around with a few front end pages. Still need to hook in the DB data and get a GIF returned to the page.

--- sandbox_42/module_1/api_controller.py
+++ sandbox_42/module_1/api_controller.py
@@ -1,9 +1,10 @@
 from datetime import datetime
-from flask import abort, request, jsonify, json
+from flask import abort, request, jsonify, json, render_template, flash, redirect, url_for
 from sandbox_42.module_1.models import Data, Logs
 from sandbox_42 import db, app
 
 __author__ = 'mhintz'
+app.secret_key = 'dawg'
 
 
 @app.route('/api/andy/<int:uuid>', methods=['POST'])
@@ -41,3 +42,29 @@ def get_payload(uuid):
     db.session.commit()
     return payload.payload, 200
 
+
+@app.route('/')
+def generate_home(name=None):
+    return render_template('base.html', name=name)
+
+
+@app.route('/home/')
+def get_gif():
+    return render_template('welcome.html')
+
+
+@app.route('/cool/thing', methods=['POST', 'GET'])
+def cool_thing():
+    error = None
+    if request.method == 'POST':
+        if request.form['password'] != app.secret_key:
+            error = 'Nice try HACKER!'
+        else:
+            flash("I think you logged in?")
+            return redirect(url_for('log_in_valid'))
+    return render_template('cool_thing.html', error=error)
+
+
+@app.route('/gif')
+def log_in_valid():
+    return render_template('giphy.html')
